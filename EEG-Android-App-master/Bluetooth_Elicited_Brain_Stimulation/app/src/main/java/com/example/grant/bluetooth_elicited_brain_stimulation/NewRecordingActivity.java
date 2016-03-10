f3665d127d1418edd9ead06fcf0db50988987a2a
Tatsumi Suenaga <suenaga.1@osu.edu>
2016-03-10 01:24:07 -0500

    Updated NewRecordActivity and its Views for that channel list and image thing

--- EEG-Android-App-master/Bluetooth_Elicited_Brain_Stimulation/app/src/main/java/com/example/grant/bluetooth_elicited_brain_stimulation/NewRecordingActivity.java
+++ EEG-Android-App-master/Bluetooth_Elicited_Brain_Stimulation/app/src/main/java/com/example/grant/bluetooth_elicited_brain_stimulation/NewRecordingActivity.java
@@ -6,12 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class NewRecordingActivity extends AppCompatActivity {
 
     private static final String TAG = NewRecordingActivity.class.getSimpleName();
     private Button mButton;
+    private ListView mChannelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,30 @@ public class NewRecordingActivity extends AppCompatActivity {
                 Log.d(TAG, "Confirm button OnClickListener");
                 Intent i = new Intent(NewRecordingActivity.this, GraphActivity.class);
                 startActivity(i);
+            }
+        });
+
+        mChannelList = (ListView) findViewById(R.id.channelList);
+        String[] channels = new String[]{"Channel 1", "Channel 2", "Channel 3",
+                "Channel 4", "Channel 5", "Channel 6", "Channel 7", "Channel 8", "Channel 9",
+                "Channel 10", "Channel 11", "Channel 12", "Channel 13", "Channel 14"};
+
+        /**First Param: Context
+         * Second Param: Layout for the Row
+         * Third Param: TextView ID
+         * Fourth Param: Array of Data
+         */
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(NewRecordingActivity.this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, channels );
+
+        //Temporary adapter just for UI, this will be later made into different fragment probably
+        mChannelList.setAdapter(adapter);
+        mChannelList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String ch = (String) mChannelList.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        ch + " is turned on", Toast.LENGTH_SHORT).show();
             }
         });
 
