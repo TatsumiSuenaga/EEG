10894476ec93b9b651fcc0bd75657b1c3a17ed65
Tatsumi Suenaga <suenaga.1@osu.edu>
2016-04-25 15:05:26 -0400

    Log out button

--- EEG-Android-App-master/Bluetooth_Elicited_Brain_Stimulation/app/src/main/java/com/example/grant/bluetooth_elicited_brain_stimulation/RecordsActivity.java
+++ EEG-Android-App-master/Bluetooth_Elicited_Brain_Stimulation/app/src/main/java/com/example/grant/bluetooth_elicited_brain_stimulation/RecordsActivity.java
@@ -1,9 +1,13 @@
 package com.example.grant.bluetooth_elicited_brain_stimulation;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,18 +18,18 @@ import java.util.ArrayList;
 public class RecordsActivity extends AppCompatActivity {
 
     private ArrayList<Recording> recordingsList;
-    private Toolbar mToolbar;
+    private final String PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_records);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        if (mToolbar != null) {
-            mToolbar.setTitle("Records");
-            setSupportActionBar(mToolbar);
-        }
+//        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+//        if (mToolbar != null) {
+//            mToolbar.setTitle("Records");
+//            setSupportActionBar(mToolbar);
+//        }
 
         ListView recordingsLV = (ListView)findViewById(R.id.listViewRecords);
 
@@ -47,6 +51,27 @@ public class RecordsActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                settings.edit().putBoolean("first_time_login", true).apply();
+                startActivity(new Intent(this,LoginActivity.class));
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
