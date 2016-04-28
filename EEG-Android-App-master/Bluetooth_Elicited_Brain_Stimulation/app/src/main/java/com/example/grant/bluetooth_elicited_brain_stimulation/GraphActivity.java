ff17b82bf38e1f61b5be350c0d660b838be34aef
confuzzed03 <kim.4256@osu.edu>
2016-04-28 19:15:41 -0400

    Patched some things up in Graph Activity

--- EEG-Android-App-master/Bluetooth_Elicited_Brain_Stimulation/app/src/main/java/com/example/grant/bluetooth_elicited_brain_stimulation/GraphActivity.java
+++ EEG-Android-App-master/Bluetooth_Elicited_Brain_Stimulation/app/src/main/java/com/example/grant/bluetooth_elicited_brain_stimulation/GraphActivity.java
@@ -38,9 +38,6 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -100,47 +97,6 @@ public class GraphActivity extends AppCompatActivity {
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Graph Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.grant.bluetooth_elicited_brain_stimulation/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Graph Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.grant.bluetooth_elicited_brain_stimulation/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 
     private class EEGDataHandler extends Handler {
 
@@ -242,8 +198,8 @@ public class GraphActivity extends AppCompatActivity {
                         } else {
                             Toast.makeText(GraphActivity.this, "Error: Cannot write to file!", Toast.LENGTH_SHORT).show();
                         }
+                        mIsRecording = !mIsRecording;
                     }
-                    mIsRecording = !mIsRecording;
                     Log.e("mIsRecording", "misrecording = " + mIsRecording);
                 } else {
                     getWritePermission();
@@ -275,9 +231,6 @@ public class GraphActivity extends AppCompatActivity {
 
             IEdk.IEE_EngineConnect(this, "");
         }
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     //adds all selected channels to the ArrayList of dataSets so they can be displayed
